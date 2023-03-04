ea86eb50846415c04b2bcf29b27c5d80ab908ada
Jollydgiant <jollydgiant@gmail.com>
2023-03-04 16:17:26 +0100

    Revert "Update webui-user.bat"
    This reverts commit fa06a8bc11e6ddbe1dd887ada8f99b2b74b43b51.

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON="C:\Users\Lance\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10\Python 3.10 (64-bit).lnk"
+set PYTHON=
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
